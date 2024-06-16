19e87da00c6ec569899368c574aaa38b261111f4
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2024-06-16 23:37:43 +0700

    Update configuration from main

--- trufflehog-exclude.sh
+++ trufflehog-exclude.sh
@@ -6,4 +6,4 @@ FILES_TO_SCAN=$(git ls-files | grep -vE "$EXCLUDED_FILES")
 
 # Run trufflehog on the filtered file list
 #trufflehog filesystem --rules trufflehog-rules.json $FILES_TO_SCAN
-$HOME/.local/bin/trufflehog filesystem trufflehog-rules.yaml --fail $FILES_TO_SCAN
+$HOME/.local/bin/trufflehog filesystem trufflehog-rules.yaml --fail $FILES_TO_SCAN --no-update
